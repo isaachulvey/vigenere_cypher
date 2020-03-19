674e8cde7b633d0c4043e7bc81facb9bda842edb
Caleb <calebf@spotify.com>
2020-03-19 14:23:44 +0100

    refactoring help

--- vigenere_cypher.py
+++ vigenere_cypher.py
@@ -7,73 +7,76 @@ import string
 # C = (M + K) mod 26
 # M = (C - K) mod 26
 
-# create base alphabet
-keys = [k for k in range(0,26)]
-values = list(ascii_uppercase)
-alphabet = dict(zip(keys, values))
+class vigenere_scyther:
+    def __init__(self, choice, message, key):
+      # create base alphabet
+      self.keys = [k for k in range(0,26)]
+      self.values = list(ascii_uppercase)
+      self.alphabet = dict(zip(self.keys, self.values))
 
-# retrieve numeric value from alphabet dict
-def get_key(val):
-	for key, value in alphabet.items():
-		if val == value:
-			return key
-	return "Key does not exist."
+      # get user inputs
+      self.choice = choice
+      self.message = self.__cleanup(message)
+      self.key = self.__cleanup(key)
 
-# remove spaces and punctuation from input
-def cleanup(text):
-	clean = text.replace(" ", "")
-	clean = clean.upper()
-	clean = clean.translate(str.maketrans('', '', string.punctuation))
-	clean = ''.join([i for i in clean if not i.isdigit()])
-	return clean
+    # retrieve numeric value from alphabet dict
+    def __get_key(self, val):
+      for key, value in self.alphabet.items():
+        if val == value:
+          return key
+      return "Key does not exist."
+        
+    # remove spaces and punctuation from input
+    def __cleanup(self, text):
+      clean = text.replace(" ", "").upper().translate(str.maketrans('', '', string.punctuation))
+      return ''.join([i for i in clean if not i.isdigit()])
 
-# take message letter and key letter, return encoded letter
-def encode(pair):
-	encoded_letter = (get_key(pair[0]) + get_key(pair[1])) % 26
-	return encoded_letter
+    # take message letter and key letter, return encoded letter
+    def __encode(self, pair):
+      return (self.__get_key(pair[0]) + self.__get_key(pair[1])) % 26
 
-# take message letter and key letter, return decoded letter
-def decode(pair):
-	decoded_letter = (get_key(pair[0]) - get_key(pair[1])) % 26
-	return decoded_letter
+    # take message letter and key letter, return decoded letter
+    def __decode(self, pair):
+      return (self.__get_key(pair[0]) - self.__get_key(pair[1])) % 26
 
-# take input message, generate [msg, key] list, encode each letter, return numerical values
-def encryption(msg, key):
-	input_list = [l for l in msg]
-	pairs = [[m, k] for m, k in zip(msg, cycle(key))]
-	for pair in pairs:
-		encoded_message = [encode(pair) for pair in pairs]
-	return encoded_message
+    # take input message, generate [msg, key] list, encode each letter, return numerical values
+    def __encryption(self):
+      input_list = [l for l in self.message]
+      pairs = [[m, k] for m, k in zip(self.message, cycle(self.key))]
+      for pair in pairs:
+        encoded_message = [self.__encode(pair) for pair in pairs]
+      return encoded_message
 
-# transform encoded message from numbers to letters
-def transformation(encoded_message):
-	output = [alphabet.get(key) for key in encoded_message]
-	return ''.join(output)
+    # transform encoded message from numbers to letters
+    def __transformation(self, method):
+      return ''.join([self.alphabet.get(k) for k in method])
+       
+    # take input message, generate [msg, key] list, decode each letter, return numerical values
+    def __decryption(self):
+      input_list = [l for l in self.message]
+      pairs = [[m, k] for m, k in zip(self.message, cycle(self.key))]
+      for pair in pairs:
+        decoded_message = [self.__decode(pair) for pair in pairs]
+      return decoded_message
 
-# take input message, generate [msg, key] list, decode each letter, return numerical values
-def decryption(msg, key):
-	input_list = [l for l in msg]
-	pairs = [[m, k] for m, k in zip(msg, cycle(key))]
-	for pair in pairs:
-		decoded_message = [decode(pair) for pair in pairs]
-	return decoded_message
+    # main, let's ride!
+    def main(self):
+      if self.choice == "1":
+        output = self.__transformation(self.__encryption())
+        print(f'Encrypted message: {output}')
+      elif self.choice == "2":
+        output = self.__transformation(self.__decryption())
+        print(f'Decrypted message: {output}')
 
-print("Welcome to Isaac's Vigenere Cypher!")
-choice = input("Enter 1 to encrypt a message or 2 to decrypt a message." )
+if __name__ == "__main__":
+  print("Welcome to Isaac's Vigenere Cypher!")
+  choice = input("Enter 1 to encrypt a message or 2 to decrypt a message: " )
+  
+  if choice not in ["1","2"]:
+    raise Exception("I asked you to enter 1 or 2, and you couldn't even do that")
+  
+  message = input("Enter your message: ")
+  key = input("Enter your key: ")
 
-if choice == "1":
-	message = input("Enter your message: ")
-	message = cleanup(message)
-	key = input("Enter your key: ")
-	key = cleanup(key)
-	output = transformation(encryption(message, key))
-	print(f"Encrypted message: {output}")
-elif choice == "2":
-	message = input("Enter your message: ")
-	message = cleanup(message)
-	key = input("Enter your key: ")
-	key = cleanup(key)
-	output = transformation(decryption(message, key))
-	print(f"Decrypted message: {output}")
-else:
-	print("Invalid choice. Please enter 1 or 2.")
+  vs = vigenere_scyther(choice, message, key)
+  vs.main()
